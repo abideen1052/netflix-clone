8819a3410d77fd85405efc09114b56e5b62ccfda
Mohammed Zainul Abideen <abideen1052@gmail.com>
2022-04-30 20:35:30 +0530

    Downloads Page is created,network images are added

--- lib/core/colors/colors.dart
+++ lib/core/colors/colors.dart
@@ -1,3 +1,7 @@
 import 'package:flutter/material.dart';
 
 const backgroundColor = Colors.black;
+const kWhiteColor = Colors.white;
+const kBlackColor = Colors.black;
+const kBlueButtonColor = Color.fromARGB(255, 44, 85, 199);
+const kWhiteButtonColor = Color.fromARGB(255, 255, 255, 255);
--- lib/presentation/downloads/screen_downloads.dart
+++ lib/presentation/downloads/screen_downloads.dart
@@ -1,12 +1,216 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:netflix_clone/core/colors/colors.dart';
+import 'package:netflix_clone/core/constatnts/constant.dart';
+import 'package:netflix_clone/presentation/widgets/app_bar_widget.dart';
 
 class ScreenDownloads extends StatelessWidget {
-  const ScreenDownloads({Key? key}) : super(key: key);
+  ScreenDownloads({Key? key}) : super(key: key);
+
+  final _widgetList = [
+    const _SmartDownloads(),
+    Section2(),
+    const Section3(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Text('Downloads'),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(50),
+        child: AppBarWidget(
+          title: 'Downloads',
+        ),
+      ),
+      body: ListView.separated(
+        padding: EdgeInsets.all(10),
+        itemBuilder: (context, index) {
+          return _widgetList[index];
+        },
+        separatorBuilder: (ctx, index) {
+          return SizedBox(
+            height: 25,
+          );
+        },
+        itemCount: _widgetList.length,
+      ),
+    );
+  }
+}
+
+class Section2 extends StatelessWidget {
+  Section2({Key? key}) : super(key: key);
+  final List imageList = [
+    "http://image.tmdb.org/t/p/w154/74xTEgt7R36Fpooo50r9T25onhq.jpg",
+    "http://image.tmdb.org/t/p/w154/okNgwtxIWzGsNlR3GsOS0i0Qgbn.jpg",
+    "http://image.tmdb.org/t/p/w154/zQSABH2Dza4mXLow2f0V2IQvJOL.jpg"
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Column(
+      children: [
+        const Text(
+          'Introducing Downloads for you',
+          style: TextStyle(
+            color: kWhiteColor,
+            fontSize: 25,
+            fontWeight: FontWeight.bold,
+          ),
+          textAlign: TextAlign.center,
+        ),
+        kHight,
+        const Text(
+          "We will download a personalised selection of\nmovies and shows for you,so there's\nalways something to watch on your\ndevice",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.grey,
+            fontSize: 17,
+            //fontWeight: FontWeight.bold,
+          ),
+        ),
+        kHight,
+        SizedBox(
+          width: size.width,
+          height: size.height,
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              CircleAvatar(
+                radius: size.width * 0.39,
+                backgroundColor: Colors.grey.withOpacity(0.5),
+              ),
+              DowloadsImageWidget(
+                imageList: imageList[0],
+                margin: const EdgeInsets.only(left: 150, bottom: 50),
+                angle: 20,
+                size: Size(size.width * 0.35, size.width * 0.52),
+              ),
+              DowloadsImageWidget(
+                imageList: imageList[1],
+                margin: const EdgeInsets.only(right: 150, bottom: 50),
+                angle: -20,
+                size: Size(size.width * 0.35, size.width * 0.52),
+              ),
+              DowloadsImageWidget(
+                imageList: imageList[2],
+                margin: const EdgeInsets.only(right: 0),
+                size: Size(size.width * 0.40, size.width * 0.61),
+                radius: 10,
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class Section3 extends StatelessWidget {
+  const Section3({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SizedBox(
+          width: double.infinity,
+          child: MaterialButton(
+            color: kBlueButtonColor,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+            ),
+            // shape: StadiumBorder(),
+            onPressed: () {},
+            child: const Padding(
+              padding: EdgeInsets.symmetric(vertical: 10),
+              child: Text(
+                'Set Up',
+                style: TextStyle(
+                    color: kWhiteColor,
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+        ),
+        kHight,
+        MaterialButton(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(6),
+          ),
+          color: kWhiteButtonColor,
+          onPressed: () {},
+          child: const Padding(
+            padding: EdgeInsets.symmetric(vertical: 10),
+            child: Text(
+              'See what you can Download',
+              style: TextStyle(
+                  color: kBlackColor,
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class _SmartDownloads extends StatelessWidget {
+  const _SmartDownloads({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: const [
+        kWidth,
+        Icon(
+          Icons.settings,
+          color: kWhiteColor,
+        ),
+        kWidth,
+        Text('Smart Downloads'),
+      ],
+    );
+  }
+}
+
+class DowloadsImageWidget extends StatelessWidget {
+  const DowloadsImageWidget({
+    Key? key,
+    required this.imageList,
+    this.angle = 0,
+    required this.margin,
+    required this.size,
+    this.radius = 10,
+  }) : super(key: key);
+
+  final String imageList;
+  final double angle;
+  final EdgeInsets margin;
+  final Size size;
+  final double radius;
+
+  @override
+  Widget build(BuildContext context) {
+    return Transform.rotate(
+      angle: angle * pi / 180,
+      child: Container(
+        margin: margin,
+        width: size.width,
+        height: size.height,
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(radius),
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: NetworkImage(imageList),
+            )),
+      ),
     );
   }
 }
